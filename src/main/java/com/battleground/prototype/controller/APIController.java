9a008b52ef13c03c83049a43076e11ed33f64fe5
nignus <getchance0@hanmail.net>
2015-08-06 10:22:29 +0900

    미사용 클래스 제거

--- src/main/java/com/battleground/prototype/controller/APIController.java
+++ src/main/java/com/battleground/prototype/controller/APIController.java
@@ -1,16 +1,11 @@
 package com.battleground.prototype.controller;
 
-import com.battleground.prototype.dao.SpringDao;
-import com.battleground.prototype.model.Article;
 import com.battleground.prototype.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import java.sql.SQLException;
 import java.util.*;
 
 /**
